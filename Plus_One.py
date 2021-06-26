93e2eaaf27b4849c5c102dc702627f731fa88cea
Sujan Peddinti <20mh1a4932@acoe.edu.in>
2021-06-26 21:27:04 +0530

    Add files via upload

---
+++ Plus_One.py
@@ -0,0 +1,16 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        num=0
+        for i in range(len(digits)):
+            num=num*10+digits[i]
+        num+=1
+        arr=list(map(int,str(num)))
+        return arr
+
+'''
+Given a non-empty array of decimal digits representing a non-negative integer, increment one to the integer.
+
+The digits are stored such that the most significant digit is at the head of the list, and each element in the array contains a single digit.
+
+You may assume the integer does not contain any leading zero, except the number 0 itself.
+'''
